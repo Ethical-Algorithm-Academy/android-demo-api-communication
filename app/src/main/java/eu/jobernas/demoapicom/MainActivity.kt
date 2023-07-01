9f72e1585a13aad44e6a04e86b60c4aa7345e258
Joao Luis <bernard.luis@necam.com>
2023-07-01 13:44:11 +0100

    refactor: Updated Main Activity to display result from Requet;

--- app/src/main/java/eu/jobernas/demoapicom/MainActivity.kt
+++ app/src/main/java/eu/jobernas/demoapicom/MainActivity.kt
@@ -5,8 +5,10 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import eu.jobernas.demoapicom.api.FlightRadarRepository
+import eu.jobernas.demoapicom.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,16 +20,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val flightRadarRepository = FlightRadarRepository()
         lifecycleScope.launch(Dispatchers.IO) {
             try {
                 val result = flightRadarRepository.flightRadarRequestsInterface.getAirCraftsList()
-                result.rows.forEach {
-                    Log.d(TAG, "Description: ${it.description}")
+                val description = result.rows.joinToString(separator = "\n") { it.description }
+                withContext(Dispatchers.Main) {
+                    binding.mainConsoleTextView.text = description
                 }
             }catch (error: Throwable) {
                 Log.e(TAG, "Error: ${error.message}", error)
+                withContext(Dispatchers.Main) {
+                    binding.mainConsoleTextView.text = error.message
+                }
             }
         }
     }
